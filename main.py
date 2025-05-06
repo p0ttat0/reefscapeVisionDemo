a7055b12b53bc04a2964ed932c998c80696dab57
p0ttat0 <ericlin.zip@gmail.com>
2025-05-06 12:57:04 -0400

    initial comit

---
+++ main.py
@@ -0,0 +1,65 @@
+from ultralytics import YOLO
+import cv2
+import  time
+
+# Load a model
+model = YOLO("best.torchscript", task="detect")  # load a custom model
+
+#camera
+cap = cv2.VideoCapture(0)
+
+assert cap.isOpened()
+
+try:
+    while True:
+        time.sleep(0.8)
+        ret, frame = cap.read()
+        assert ret
+
+        #x = 0.32
+        #frame = frame[:, round(1280 * x):round(1280 * (1 - x))].transpose(1, 0, 2)
+
+        # Inference
+        predictions = model(frame, imgsz=[256, 320])
+
+        for prediction in predictions:
+            # Extract all the box coords, class ids, and confidences
+            xyxy = prediction.boxes.xyxy.cpu().numpy()  # shape: [num_boxes, 4]
+            cls_ids = prediction.boxes.cls.int().cpu().numpy()
+            confs = prediction.boxes.conf.cpu().numpy()
+
+            # Loop through detections and draw
+            for (x1, y1, x2, y2), cls_id, conf in zip(xyxy, cls_ids, confs):
+                if confs > 0.7:
+                    break
+
+                x1, y1, x2, y2 = map(int, (x1, y1, x2, y2))
+                label = f"{prediction.names[cls_id]} {conf:.2f}"
+
+                # Draw rectangle
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), thickness=2)
+
+                # Draw filled box behind text for readability
+                (w, h), _ = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+                cv2.rectangle(frame, (x1, y1 - int(1.2 * h)), (x1 + w, y1), (0, 255, 0), -1)
+
+                # Put the class+conf text
+                cv2.putText(
+                    frame,
+                    label,
+                    (x1, y1 - 2),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    (0, 0, 0),
+                    thickness=1,
+                    lineType=cv2.LINE_AA
+                )
+
+            # Display the image in a window
+            cv2.imshow('YOLOv8 Detections', frame)
+        # Press 'q' to exit
+        if cv2.waitKey(0) & 0xFF == ord('q'):
+            break
+finally:
+    cap.release()
+    cv2.destroyAllWindows()
